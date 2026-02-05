ba7cca4822081a3808ea220f6db8397d1784e511
Jyoti Singh <jyotisingh82000@gmail.com>
2026-02-05 19:27:35 +0530

    Structured Output with Few SHot Prompting

--- prompts/few.py
+++ prompts/few.py
@@ -9,17 +9,38 @@ client = OpenAI(
     api_key= os.environ.get("GOOGLE_API_KEY"),
     base_url= "https://generativelanguage.googleapis.com/v1beta/"
 )
+# SYSTEM_PROMPT = """
+#     you should only and only answer the coding related questions. Do not  answer anythin else. Your name is Alexa . If user asks something otehr than coding , just say sorry.
+    
+#     Examples:
+#     Q: Can you explain the a +b whole square?
+#     A: Sorry, i can only help with coding related questions.
+    
+#     Q: Hey , Write a code in python for adding two numbers.
+#     A: def(a, b):
+#           return a+b
+# """
 
 SYSTEM_PROMPT = """
     you should only and only answer the coding related questions. Do not  answer anythin else. Your name is Alexa . If user asks something otehr than coding , just say sorry.
     
+    
+    Rule:
+    - Strictly follow the output in JSON format
+    
+    Output Format:
+    {{
+        "code" : "string" or null,
+        "isCpodingQuestion" : boolean
+    }}
+    
     Examples:
     Q: Can you explain the a +b whole square?
-    A: Sorry, i can only help with coding related questions.
+    A: {{ "code" : null, "isCpodingQuestion" : false }}
     
     Q: Hey , Write a code in python for adding two numbers.
-    A: def(a, b):
-          return a+b
+    A: {{"code" : "def(a, b):
+                     return a+b", "isCpodingQuestion" : true}}
 """
 
 response = client.chat.completions.create(
@@ -27,7 +48,7 @@ response = client.chat.completions.create(
     n=1,
     messages= [
         {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": "Hey, Can you explain a+b whole squre."}
+        {"role": "user", "content": "Hey, write a code to add n numbers in js"}
     ]
 )
 
